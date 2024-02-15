1ad8e8e8544f87b26b67a5e983ee9e831971379b
Omkar <okumbhar009gmai@ip-172-31-42-111.ec2.internal>
2024-02-15 16:23:56 +0000

    bug fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 
-public class HelloWord{
-           public static void(string[] args){
+public class HelloWorld{
+           public static void(string[]args){
                     System.out.println("Hello,World!");
               }
 }
